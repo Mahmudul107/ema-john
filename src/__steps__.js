b07ddc5aeb8673af5a894230e0316c623143d7fd
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-16 23:49:53 +0600

    pagination with server completed

---
+++ src/__steps__.js
@@ -0,0 +1,48 @@
+/*
+* 1. Create firebase Projects
+* 2. Create web app
+* 3. Install firebase : npm i firebase
+* 4. Save firebase config and export default app
+* 5. Build > Authentication > Get started > Enable sign in method
+* 6. Create sign up and login route
+* 7. 
+*
+*/ 
+
+
+/*
+* 1. Create a context provider file
+* 2. Create a context and set Provider
+* 3. Set the children props
+* 4. Set context value
+* 5. Set the provider in main.jsx
+* 6.
+*/ 
+
+
+/*
+* HOSTING
+*
+* 1. One time install per PC : 
+* 2 npm install -g firebase-tools
+* 3. firebase login
+
+
+-----------------
+* Each projects for one time
+* 1. firebase init
+* 2. Proceed
+* 3. Hosting: Firebase ( up and down arrow to change the question) , use space bar to select. 
+* 4. Existing project
+* 5. Select the project carefully
+* 6. Which project as public directory : VITE : Dist
+* 7. Single page application : Yes
+* 8. Continuous deployment : No
+* 9. 
+*
+* FOR EVERY TIME DEPLOY
+* 1. npm run build
+* 2. firebase deploy
+*
+*
+ */ 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaFNA04b9tfbmUfu31L6knjo7H8noPTWM",
+  authDomain: "ema-john-with-firebase-a-4f595.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-4f595",
+  storageBucket: "ema-john-with-firebase-a-4f595.appspot.com",
+  messagingSenderId: "584291077325",
+  appId: "1:584291077325:web:9b4ad0ceb95caa0be32d84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
